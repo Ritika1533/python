65a46904160a6bbcf1c46e7e12c3b1f12cba921f
unknown <ritikakes33@gmail.com>
2024-06-29 05:19:55 +0530

    python

---
+++ Learning/first.py
@@ -0,0 +1,45 @@
+print("hello ritika")
+#print statement : want to print in different line use different print statement
+print("started to learn python?? ","yes/no")
+#number
+print(45)
+#addation
+print(23+78)
+#varible
+name="ritika"
+age=67
+price=56.97
+print("My name is : ", name)
+print("Price of pencl : ", price)
+print("My age is : ", age)
+#assignment
+age2=age
+print(age2)
+#data type
+print(type(name))
+print(type(price))
+#boolean
+old=False
+a=None
+print(type(old))
+print(old)
+print(type(a))
+#print sum
+c=78
+d=56
+sum=c+d
+print("sum of two number : ",sum)
+#type conversion
+x=7
+y=4.7
+sum2=x+y # 7.0 + 4.7
+print(sum2)
+#type casting
+n=int("8")
+m=7.9
+print(n+m)
+#input in python
+title=input("enter your title : ")
+print(title)
+score=int(input("enter your scode : "))
+print(type(score))
---
+++ Learning/question1.py
@@ -0,0 +1,22 @@
+#QUESTION 1 : uer input of two numbr and print the sum of two number
+
+num1=int(input("enter first number : "))
+num2=int(input("enter second number : "))
+sum=num1+num2
+print("the sum of two number is : ",sum)
+
+#QUESTION 2 : input square side and print the area
+
+side=int(input("enter the side of square : "))
+area=4*side
+print("area oof square : ", area)
+
+#QUESTION 3 : user input two float number and print the average value
+
+num3=float(input("enter first number : "))
+num4=float(input("enter second number : "))
+average1=(num3+num4)/2
+print("average value : ",average1)
+
+#QUESTION 4 : print true if num1>=num2 and false if num1 is not >= b
+print("is num1>=num2 ? ",(num1>=num2))
